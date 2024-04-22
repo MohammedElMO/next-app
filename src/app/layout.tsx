e3a8893bac0cb79fafff3b5921b66814c4d5a23b
MohammedElMO <elamranimohammed104@gmail.com>
2024-04-22 14:27:49 +0100

    allowing google analytics to hook in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import AuthProvider from "./auth/AuthProvider";
 import NavBar from "./components/NavBar";
+import Script from "next/script";
 
 const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -20,13 +21,30 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
+      <Script id="googleAnalytics">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MV53LZRD');`}
+      </Script>
       <body
-
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MV53LZRD"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          ></iframe>
+        </noscript>
         <AuthProvider>
           <NavBar />
           {children}
